8529e7d46a98b0b7d97dcbf4eeac4836f9bd7e86
Brenda-Caroline <2014brendacs0@gmail.com>
2021-12-17 21:40:49 -0300

    Cria funções

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ if (idade >= 18) {
 
 /* ESTRUTURA DE REPETIÇÃO */
 
-var count = 0;
+/*var count = 0;
 while(count <= 5) {
     console.log(count)
     count = count +1;
@@ -58,15 +58,74 @@ while(count <= 5) {
 var count;
 for(count=0; count <= 5; count ++) {
     console.log(count);
-}
+}*/
 
 
 /*INFORMAÇÃO DE DIA E HORÁRIO */
 
-var d = new Date();
+/*var d = new Date();
 console.log(d);
 console.log("Hoje é " +d.getDate()+ "/" + d.getMonth()+1);
 console.log(d.getDay());
 console.log(d.getMonth()+1);
 console.log(d.getHours());
 console.log(d.getMinutes());
+*/
+
+
+/* FUNÇÕES */
+
+function clicou() {
+    document.getElementById("agradecimento").innerHTML="<b>Obrigado por clicar</b>";
+
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar() {
+    window.open("https://globallabs.academy/"); //Abre nova página em outra guia
+    //window.location.href = "https://globallabs.academy/"; //Abre nova página na mesma guia
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML="Obrigado por passar o mouse";
+    //alert("Trocar texto");
+    elemento.innerHTML= "Obrigado por passar o mouse";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemoveover").innerHTML="Passe o mouse aqui";
+    elemento.innerHTML= "Passe o mouse aqui";
+}
+
+function load(){
+    alert("Página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+/* FUNÇÃO SOMA */
+/*function soma(n1, n2) {
+    return n1 + n2;
+}
+
+alert(soma(5, 10));*/
+
+/* FUNÇÃO TROCA PALAVRA */
+/*function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+
+alert(setReplace("Vai Japão", "Japão", "Brasil"));*/
+
+/* FUNÇÃO COM VARIÁVEL LOCAL */
+/*function validaIdade(idade) {
+    if (idade >= 18) {
+        return true
+    } else {
+        return false
+    }
+}
+
+var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));*/
